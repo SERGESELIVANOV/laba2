d05e5f98e838ce2148cddb303a979906d24bf16e
Sergey <ser02102005@gmail.com>
2024-11-21 23:05:43 +0500

    ночальный каркас

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void Read(vector <string> vektor1)
+{
+
+}
+void Output(vector <string> vektor1)
+{
+
+}
+void Writing(vector <string> vektor1)
+{
+
+}
+
+int main()
+{
+    vector <string> vektor1;
+    Read(vektor1);
+    Output(vektor1);
+    Writing(vektor1);
+    cin.get();
+    return 0;
+}
